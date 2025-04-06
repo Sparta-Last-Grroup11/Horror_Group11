c8bba035f1d3da93cdc9e2edcee2978b8f52b7d1
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-04-07 01:45:37 +0900

    Chore: 스켈레톤 코드 및 한글 인코딩 추가

---
+++ Assets/Script/Manager/EventManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : Singleton<EventManager>
+{
+    public bool IsEventRun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Player/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public P_Movement PlayerMove;
+    public P_Interaction PlayerInter;
+    //상태 머신은 알아서.
+
+    public void OnDamage()
+    {
+
+    }
+}
