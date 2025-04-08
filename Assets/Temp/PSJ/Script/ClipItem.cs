acd015030410049bd49ab325785876a4e59ce5be
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-04-08 11:10:42 +0900

    [WIP] 박성준 중간 정리

---
+++ Assets/Temp/PSJ/Script/ClipItem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class ClipItem : MonoBehaviour, I_Interactable
+{
+    [SerializeField] string description;
+
+    TextMeshProUGUI text;
+
+    public void Awake()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        text.text = description;
+    }
+
+    public void OnInteraction()
+    {
+        
+    }
+}
