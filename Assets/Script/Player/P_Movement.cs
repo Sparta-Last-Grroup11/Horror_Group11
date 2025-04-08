8e6f4fdac62946024fed8a1de91d821261716608
Najaehyeon <jaehyeon4832@gmail.com>
2025-04-08 16:46:59 +0900

    Feat: 플레이어 달리기 기능
    Left Shift눌러서 달리고,
    움직일 때, 화면 흔들림 효과 추가

--- Assets/Script/Player/P_Movement.cs
+++ Assets/Script/Player/P_Movement.cs
@@ -1,19 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class P_Movement : MonoBehaviour
+public class P_Movement : P_StateMachine
 {
-    [SerializeField] private float speed;
-    // Start is called before the first frame update
-    void Start()
+    public P_Movement(Player player) : base(player) { }
+
+    public override void Enter()
     {
-        
+        Debug.Log("Movement 상태 시작");
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        
+        HandleLook();
+        HandleMove();
+        HandleCameraShake();
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Movement 상태 종료");
+    }
+
+    private void HandleLook()
+    {
+        _player.curRotX -= _player.lookInput.y * _player.lookSensitivity;
+        _player.curRotX = Mathf.Clamp(_player.curRotX, _player.minXLook, _player.maxXLook);
+        _player.curRotY += _player.lookInput.x * _player.lookSensitivity;
+
+        _player.cameraContainer.localEulerAngles = new Vector3(_player.curRotX, 0, 0);
+        _player.transform.eulerAngles = new Vector3(0, _player.curRotY, 0);
+    }
+
+    private void HandleMove()
+    {
+        Vector3 moveDir =
+            (_player.transform.forward * _player.moveInput.y +
+            _player.transform.right * _player.moveInput.x) * (_player.runPressing ? _player.runSpeed : 1f);
+
+        if (_player.CharacterController.isGrounded)
+        {
+            _player.verticalVelocity = _player.jumpPressed ? _player.jumpPower : moveDir.y;
+            _player.jumpPressed = false;
+        }
+        else
+        {
+            _player.verticalVelocity += _player.gravity * Time.deltaTime;
+        }
+
+        moveDir.y = _player.verticalVelocity;
+        _player.CharacterController.Move(moveDir * _player.moveSpeed * Time.deltaTime);
+
+        bool isMoving = _player.moveInput.magnitude > 0.2f;
+        if (isMoving && _player.CharacterController.isGrounded)
+        {
+            float intensity = _player.runPressing ? _player.runShakeIntensity : _player.walkShakeIntensity;
+            float frequency = _player.runPressing ? _player.runShakeFrequency : _player.walkShakeFrequency;
+
+            StartCameraShake(intensity, frequency, _player.shakeDuration);
+        }
+    }
+
+    private void HandleCameraShake()
+    {
+        if (_player.shakeTimer > 0)
+        {
+            _player.shakeTimer -= Time.deltaTime;
+            if (_player.shakeTimer <= 0 && _player.camNoise != null)
+            {
+                _player.camNoise.m_AmplitudeGain = 0f;
+                _player.camNoise.m_FrequencyGain = 0f;
+            }
+        }
+    }
+
+    private void StartCameraShake(float intensity, float frequency, float duration)
+    {
+        if (_player.camNoise != null)
+        {
+            _player.camNoise.m_AmplitudeGain = intensity;
+            _player.camNoise.m_FrequencyGain = frequency;
+            _player.shakeTimer = duration;
+        }
     }
 }
--- Assets/Script/Player/StateMachine/P_StateMachine.cs
+++ Assets/Script/Player/StateMachine/P_StateMachine.cs
@@ -1,8 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class P_StateMachine : MonoBehaviour
 {
-    
+    protected Player _player;
+
+    public P_StateMachine (Player player)
+    {
+        _player = player;
+    }
+
+    public abstract void Enter();
+    public abstract void Update();
+    public abstract void Exit();
 }
