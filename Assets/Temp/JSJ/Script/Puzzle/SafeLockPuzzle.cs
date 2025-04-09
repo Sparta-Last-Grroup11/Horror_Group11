09d3071beb2aa0892c032a928a0c4b794b9ad9d1
tmdwp <tmdwp1010@gmail.com>
2025-04-09 11:28:26 +0900

    Merge branch 'Dev' into JSJ_Puzzle

---
+++ Assets/Temp/JSJ/Script/Puzzle/SafeLockPuzzle.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeLockPuzzle : MonoBehaviour
+{
+    [SerializeField] private int[] rightArray = {0,0,0,0};
+    [SerializeField] private OpenDoor door;
+    [SerializeField] private AudioSource audio;
+    [SerializeField] private Surprise surprise;
+    [SerializeField] private int randomRange;
+
+    private int rotateCount = 0;
+ //   [HideInInspector]
+    public int answer = 0;
+    private int maxNumber = 9;
+    private int arrayLength;
+    public bool isLeft;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        for (int i = 0; i < rightArray.Length; i++) //암호 초기화
+        {
+            rightArray[i] = Random.Range(0,maxNumber+1);
+            arrayLength = rightArray.Length ;
+        }
+        //gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        RotateDial();
+    }
+    public void RotateDial() //다이얼 돌리기
+    {
+        if (Input.GetKeyDown(KeyCode.A)) //왼쪽으로 회전
+        {
+            if (isLeft) //직전에 왼쪽으로 회전시켰을 때
+            {
+                answer = (answer + 1) % (maxNumber + 1);
+                if (answer == rightArray[rotateCount])
+                    audio.Play();
+            }
+            else  //직전에 오른쪽으로 돌렸다면
+            {
+                CheckAnswer(); //돌린 횟수가 암호와 일치하는지 확인
+                answer = (answer + 1) % (maxNumber + 1);
+            }
+            if (Random.Range(0, randomRange) == 0)
+            {
+                surprise.SurpriseSound();
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.D)) //오른쪽으로 회전
+        {
+            if (!isLeft) //직전에 오른쪽으로 회전했을 때
+            {
+                answer = (answer + 1) % (maxNumber + 1);
+                if (answer == rightArray[rotateCount])
+                    audio.Play();
+            }
+            else //직전에 왼쪽으로 회전했을 때
+            {
+                CheckAnswer(); //돌린 횟수가 암호와 일치하는지 확인
+                answer = (answer + 1) % (maxNumber + 1);
+            }
+            if (Random.Range(0, randomRange) == 0)
+            {
+                surprise.SurpriseSound();
+            }
+        }
+        if (rotateCount == arrayLength - 1 && answer == rightArray[arrayLength - 1]) // 마지막 암호까지 맞췄다면 문 열기
+        {
+            SuccessOpen();
+        }
+    }
+
+    void CheckAnswer() //현재 입력이 암호와 일치한지
+    {
+        if (answer == rightArray[rotateCount])
+        {
+            answer = 0;
+            rotateCount++;
+            isLeft = !isLeft;
+        }
+        else //일치하지 않는다면 전부 초기화
+        {
+            answer = 0;
+            rotateCount = 0;
+            isLeft = false;
+        }
+    }
+
+    void SuccessOpen() // 비밀번호를 맞췄을 경우
+    {
+        door.OpenTheDoor();
+        LockCursor();
+        gameObject.SetActive(false);
+    }
+    void LockCursor() //커서 고정 해제
+    {
+        bool toggle = Cursor.lockState == CursorLockMode.Locked;
+        Cursor.lockState = toggle ? CursorLockMode.None : CursorLockMode.Locked;
+    }   
+}
---
+++ Assets/Temp/PSJ/Script/AudioManager.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using Unity.VisualScripting;
+using UnityEngine.Pool;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    [SerializeField] SoundSource curBgm;
+
+    ObjectPool<SoundSource> audioPool;
+    [SerializeField] SoundSource audioSourcePrefab;
+
+    [SerializeField] Transform tempRoot;
+    [SerializeField] Transform bgmRoot;
+
+    void Awake()
+    {
+        // 풀 초기화
+        audioPool = new ObjectPool<SoundSource>(
+            CreatePooledItem,
+            OnTakeFromPool,
+            OnReturnedToPool,
+            OnDestroyPoolObject,
+            collectionCheck: false,
+            defaultCapacity: 10,
+            maxSize: 20
+        );
+        tempRoot = new GameObject("Temp").transform;
+        tempRoot.SetParent(transform);
+
+        bgmRoot = new GameObject("BGM").transform;
+        bgmRoot.SetParent(transform);
+    }
+
+    #region 풀링
+    SoundSource CreatePooledItem()
+    {
+        SoundSource soundSource = Instantiate(audioSourcePrefab).GetComponent<SoundSource>();
+        soundSource.Init(audioPool);
+        return soundSource;
+    }
+
+    void OnTakeFromPool(SoundSource obj)
+    {
+        obj.gameObject.SetActive(true);
+    }
+
+    void OnReturnedToPool(SoundSource obj)
+    {
+        obj.gameObject.SetActive(false);
+    }
+
+    void OnDestroyPoolObject(SoundSource obj)
+    {
+        Destroy(obj.gameObject);
+    }
+
+    #endregion
+    public void Audio2DPlay(AudioClip clip)
+    {
+        //게임 오브젝트를 만든 후에 오디오 소스를 붙여서 clip을 재생
+        if (clip == null)
+        {
+            Debug.LogWarning("재생할 오디오 클립이 없습니다.");
+            return;
+        }
+
+        SoundSource obj = audioPool.Get();
+        obj.transform.SetParent(tempRoot);
+
+        AudioSource source = obj.GetComponent<AudioSource>();
+        source.clip = clip;
+        source.spatialBlend = 0f;
+        source.Play();
+
+        // clip의 길이만큼 지난 후 오브젝트 파괴
+        obj.GetComponent<SoundSource>().Play(clip.length);
+    }
+
+    public AudioSource Audio3DPlay(AudioClip clip, Vector3 pos, bool isLoop = false)
+    {
+        if (clip == null)
+        {
+            Debug.LogWarning("재생할 오디오 클립이 없습니다.");
+            return null;
+        }
+
+        SoundSource obj = audioPool.Get();
+        obj.transform.position = pos;
+        obj.transform.SetParent(tempRoot);
+
+        AudioSource source = obj.GetComponent<AudioSource>();
+        source.clip = clip;
+        source.loop = isLoop;
+        source.spatialBlend = 1f;
+        source.Play();
+
+        if (!isLoop) 
+            obj.Play(clip.length);
+        return source;
+    }
+
+    public void AudioBGMPlay(AudioClip clip, bool isLoop = true)
+    {
+        if (clip == null)
+        {
+            Debug.LogWarning("재생할 BGM 클립이 없습니다.");
+            return;
+        }
+
+        if (curBgm != null)
+        {
+            audioPool.Release(curBgm);
+            curBgm = null;
+        }
+        SoundSource obj = audioPool.Get();
+        obj.transform.SetParent(bgmRoot);
+
+        AudioSource source = obj.GetComponent<AudioSource>();
+        source.clip = clip;
+        source.spatialBlend = 0f;
+        source.loop = isLoop;
+        source.Play();
+
+        if (isLoop == true)
+            curBgm = obj;
+        else
+            obj.Play(clip.length);
+    }
+
+    public void StopBGM()
+    {
+        if (curBgm != null)
+        {
+            audioPool.Release(curBgm);
+            curBgm = null;
+        }
+    }
+}
