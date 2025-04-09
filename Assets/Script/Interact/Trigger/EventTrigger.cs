8f6cbd890ae17905ce930c6ee526f674f72f6de0
tmdwp <tmdwp1010@gmail.com>
2025-04-09 11:23:22 +0900

    [Feat] 전등 깜빡임
    Player 태그 오브젝트와 충돌 시 주변의 Light 탐색 후 깜빡임

--- Assets/Script/Interact/Trigger/EventTrigger.cs
+++ Assets/Script/Interact/Trigger/EventTrigger.cs
@@ -8,4 +8,12 @@ public class EventTrigger : MonoBehaviour
     //or 스크립트 하나에 Ontrigger or OverlapBox
     //이 스크립트가 아마 Ontrigger 역할
     //변수는 이제 Receiver를 Serialize로 할당하고 Receiver에서 받아서 이벤트 작동.
+    [SerializeField] private Receiver receiver;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            receiver.ReceiveTrigger();
+        }
+    }
 }
