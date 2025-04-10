ae40fd1a4d3f3cce45f16fb9b4e50b0050f3d54a
tmdwp <tmdwp1010@gmail.com>
2025-04-10 16:25:44 +0900

    Feat 전등 On/Off
    전등 On/Off 구현
    문 닫기 구현

---
+++ Assets/Script/Interact/Door/ControlDoor.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlDoor : MonoBehaviour
+{
+    [SerializeField] private AudioSource aduioSource;
+
+    public float duration = 1f;
+    public void OpenTheDoor()
+    {
+        StartCoroutine(OpenDoorMotion());
+    }
+
+
+    IEnumerator OpenDoorMotion() //문 회전
+    {
+        aduioSource.Play();
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = startRotation * Quaternion.Euler(0, -90, 0);
+
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = endRotation;
+    }
+
+
+    public void CloseTheDoor()
+    {
+        StartCoroutine(CloseDoorMotion());
+    }
+
+
+    IEnumerator CloseDoorMotion() //문 회전
+    {
+        aduioSource.Play();
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = startRotation * Quaternion.Euler(0, 90, 0);
+
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.rotation = endRotation;
+    }
+}
