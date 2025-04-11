10bd3846d732794d0aa0ed88de267303e7e0c36e
Najaehyeon <jaehyeon4832@gmail.com>
2025-04-11 20:29:37 +0900

    Feat: PopupUI에 FadeIn, FadeOut기능 추가
    메서드 정의만 했고, 사용방식에 따라 수정 필요

--- Assets/Script/UI/PopupUI.cs
+++ Assets/Script/UI/PopupUI.cs
@@ -1,4 +1,58 @@
+using System.Collections;
+using UnityEngine;
+
 public class PopupUI : BaseUI
 {
-    
+    private CanvasGroup canvasGroup;
+
+    [SerializeField] private float duration = 0.5f;
+    private float elapsed = 0f;
+
+    private void OnEnable()
+    {
+        gameObject.AddComponent<CanvasGroup>();
+
+        if (canvasGroup == null)
+            canvasGroup = GetComponent<CanvasGroup>();
+
+        StartCoroutine(FadeIn());
+    }
+
+    private IEnumerator FadeIn()
+    {
+        canvasGroup.alpha = 0f;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Clamp01(elapsed / duration);
+            yield return null;
+        }
+
+        canvasGroup.alpha = 1f;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    private IEnumerator FadeOut()
+    {
+        elapsed = duration;
+        canvasGroup.alpha = 1f;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+
+        while (elapsed > 0)
+        {
+            elapsed -= Time.deltaTime;
+            canvasGroup.alpha = Mathf.Clamp01(elapsed / duration);
+            Debug.Log("어두워 지는 중");
+            yield return null;
+        }
+
+        canvasGroup.alpha = 0f;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
 }
--- Assets/Temp/NJH/Scripts/PopupInventory.cs
+++ Assets/Temp/NJH/Scripts/PopupInventory.cs
@@ -12,45 +12,12 @@ public class PopupInventoryUI : PopupUI
     [SerializeField] private TextMeshProUGUI itemDescription;
     [SerializeField] private Image manualImage; // 조작법 이미지
 
-    private CanvasGroup canvasGroup;
-
     private void Start()
     {
-        canvasGroup = GetComponent<CanvasGroup>();
-
         for (int i = 0; i < slots.Count; i++)
         {
             slots[i].transform.SetParent(itemSlotGroup.transform);
             slots[i].transform.SetAsLastSibling();
         }
-    }
-
-    private void OnEnable()
-    {
-        if (canvasGroup == null)
-            canvasGroup = GetComponent<CanvasGroup>();
-
-        StartCoroutine(FadeIn());
-    }
-
-    private IEnumerator FadeIn()
-    {
-        float duration = 0.5f;
-        float elapsed = 0f;
-
-        canvasGroup.alpha = 0f;
-        canvasGroup.interactable = false;
-        canvasGroup.blocksRaycasts = false;
-
-        while (elapsed < duration)
-        {
-            elapsed += Time.deltaTime;
-            canvasGroup.alpha = Mathf.Clamp01(elapsed / duration);
-            yield return null;
-        }
-
-        canvasGroup.alpha = 1f;
-        canvasGroup.interactable = true;
-        canvasGroup.blocksRaycasts = true;
     }
 }
