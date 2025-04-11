40170d7f7c3546bca139983aaccb2413d7c64e51
cookie35 <hakyeong35@gmail.com>
2025-04-11 12:12:46 +0900

    Feat: Snailman Test용 씬 생성 및 스크립트 일부 수정

--- Assets/Script/Enemy/01 Core/Enemy.cs
+++ Assets/Script/Enemy/01 Core/Enemy.cs
@@ -4,13 +4,14 @@ public abstract class Enemy : MonoBehaviour
 {
     protected E_StateMachine fsm;
 
-    public Transform Player;
+    protected Transform playerTransform;
     public LayerMask playerLayer;
     [SerializeField] private float viewAngle = 90f;  
 
     protected virtual void Awake()
     {
         fsm = new E_StateMachine();
+        playerTransform = GameManager.Instance.player.transform;
 
         if (playerLayer == 0)
             playerLayer = LayerMask.GetMask("Player");
@@ -23,7 +24,7 @@ public abstract class Enemy : MonoBehaviour
 
     public bool CanSeePlayer()
     {
-        Vector3 dirToPlayer = (Player.position - transform.position).normalized;  //  몬스터에서 플레이어로 가는 방향 벡터
+        Vector3 dirToPlayer = (playerTransform.position - transform.position).normalized;  //  몬스터에서 플레이어로 가는 방향 벡터
         float angle = Vector3.Angle(transform.forward, dirToPlayer);  // 내가 보고 있는 방향과 플레이어 방향의 각도를 계산 
 
         if (angle < viewAngle / 2f)  // 왼쪽 오른쪽 각도 나누고
