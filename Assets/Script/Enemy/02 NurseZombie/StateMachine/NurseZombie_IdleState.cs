850ea5f6a5e275a044c3abd51bb00460e41cd7b1
cookie35 <hakyeong35@gmail.com>
2025-04-14 16:46:01 +0900

    Chore: zombie들 레이어 수정 및 변수명 통일

--- Assets/Script/Enemy/02 NurseZombie/StateMachine/NurseZombie_IdleState.cs
+++ Assets/Script/Enemy/02 NurseZombie/StateMachine/NurseZombie_IdleState.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class NurseZombie_IdleState : E_BaseState  // 기본 상태일 때
 {
-    private NurseZombie nurse;
+    private NurseZombie nurseZombie;
 
     public NurseZombie_IdleState(Enemy enemy, E_StateMachine fsm) : base(enemy, fsm)
     {
-        nurse = enemy as NurseZombie;
+        nurseZombie = enemy as NurseZombie;
     }
 
     public override void Enter()
     {
-        nurse.nurseAnimator.SetBool("IsChasing", false);
+        nurseZombie.nurseAnimator.SetBool("IsChasing", false);
     }
 
     public override void Update()
@@ -19,14 +19,14 @@ public class NurseZombie_IdleState : E_BaseState  // 기본 상태일 때
         Debug.Log("IdleState: Update 진입");
 
         bool canSee = enemy.CanSeePlayer();
-        bool isPlayerLooking = nurse.IsPlayerLookingAtMe();
+        bool isPlayerLooking = nurseZombie.IsPlayerLookingAtMe();
 
         Debug.Log($"CanSeePlayer: {canSee}, IsPlayerLookingAtMe: {isPlayerLooking}");
 
         if (canSee && !isPlayerLooking)
         {
             Debug.Log("추적상태로 전환");
-            fsm.ChangeState(new NurseZombie_ChaseState(nurse, fsm));
+            fsm.ChangeState(new NurseZombie_ChaseState(nurseZombie, fsm));
         }
     }
 }
