162430807aa6ac6c27f77abfc6ab7d129f063dbb
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-04-14 15:21:10 +0900

    Feat: 인벤토리 UI 틀 구현

--- Assets/Temp/NJH/Scripts/PopupInventory.cs
+++ Assets/Temp/NJH/Scripts/PopupInventory.cs
@@ -6,18 +6,76 @@ using UnityEngine.UI;
 
 public class PopupInventoryUI : PopupUI
 {
+    [SerializeField] private RawImage image;
     [SerializeField] private TextMeshProUGUI inventoryTitle;
     [SerializeField] private GameObject itemSlotGroup;
-    [SerializeField] private List<GameObject> slots; // 갖고 있는 아이템으로 변경 필요
+    [SerializeField] private List<Slot> slots; // 갖고 있는 아이템으로 변경 필요
     [SerializeField] private TextMeshProUGUI itemDescription;
     [SerializeField] private Image manualImage; // 조작법 이미지
+    [Header("Rotate")]
+    public float rotationSpeed = 20f;
 
-    private void Start()
+    private bool isDragging = false;
+    private Vector3 lastMousePosition;
+    GameObject cur3DObject;
+
+    protected override void Start()
     {
+        base.Start();
+        UIManager.Instance.IsUiActing = true;
         for (int i = 0; i < slots.Count; i++)
         {
+            /*
             slots[i].transform.SetParent(itemSlotGroup.transform);
             slots[i].transform.SetAsLastSibling();
+            */
+            var slot = slots[i];
+            slot.TryGetComponent<Button>(out Button but);
+            if (but != null)
+                but.onClick.AddListener(() => {
+                    if (UIManager.Instance.cur3DObject != null)
+                        UIManager.Instance.RemovePrefabInSumCam();
+                    cur3DObject = UIManager.Instance.MakePrefabInSubCam(slot.Object3D); // 이제 안전!
+                });
+        }
+    }
+
+    protected void Update()
+    {
+        Rotate();
+    }
+
+    protected override void OnDestroy()
+    {
+        UIManager.Instance.RemovePrefabInSumCam();
+    }
+
+    void Rotate()
+    {
+        if (cur3DObject == null)
+            return;
+        // 마우스 왼쪽 버튼을 누르면 드래그 시작
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+            lastMousePosition = Input.mousePosition;
+        }
+
+        // 마우스 버튼 떼면 드래그 종료
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        // 드래그 중일 때 회전 처리
+        if (isDragging)
+        {
+            Vector3 delta = Input.mousePosition - lastMousePosition;
+            float rotationY = -delta.x * rotationSpeed * Time.deltaTime;
+
+            cur3DObject.transform.Rotate(0, rotationY, 0f, Space.Self);
+
+            lastMousePosition = Input.mousePosition;
         }
     }
 }
