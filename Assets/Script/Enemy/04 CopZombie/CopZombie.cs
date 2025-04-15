81c51b6d15a245e12e9b7b8960f840b81dbda459
Najaehyeon <jaehyeon4832@gmail.com>
2025-04-15 20:39:10 +0900

    Feat: 경비 좀비가 문여는 기능

--- Assets/Script/Enemy/04 CopZombie/CopZombie.cs
+++ Assets/Script/Enemy/04 CopZombie/CopZombie.cs
@@ -9,6 +9,12 @@ public class CopZombie : Enemy
     public Animator copZombieAnim;
     public float patrolRange = 20f;
 
+    // Door
+    private float detectDoorRate = 0.5f;
+    private float afterDetectDoor;
+    private float detectDoorRange = 1f;
+    [SerializeField] private LayerMask doorLayerMask;
+
     private void Awake()
     {
         copzombieAgent = GetComponent<NavMeshAgent>();
@@ -20,5 +26,28 @@ public class CopZombie : Enemy
         base.Start();
         fsm = new E_StateMachine();
         fsm.ChangeState(new CopZombie_PatrolState(this, fsm));
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        Debug.DrawRay(transform.position + Vector3.up, transform.forward, Color.red, 1f);
+
+        afterDetectDoor += Time.deltaTime;
+        if (afterDetectDoor >= detectDoorRate)
+        {
+            Ray ray = new Ray(transform.position + Vector3.up, transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, detectDoorRange, doorLayerMask))
+            {
+                ControlDoor door = hit.collider.GetComponent<ControlDoor>();
+                if (door != null)
+                {
+                    door.OpenTheDoor();
+                }
+            }
+            afterDetectDoor = 0;
+        }
     }
 }
