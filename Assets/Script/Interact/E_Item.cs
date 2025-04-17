c89b31130f8c7af9e563c0c627cb9d4263507ba0
tmdwp <tmdwp1010@gmail.com>
2025-04-17 11:06:42 +0900

    Refactor 아이템 Enum 수정

---
+++ Assets/Script/Interact/E_Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum E_Item
+{
+    None = -1,
+    Battery = 0, BoxInDiningRoom,
+    PowerRoomKey = 100, MovieRoomKey, FrontDoorKey,
+    FileA = 200, FileB, MemoInFront, MemoInOffice,
+}
