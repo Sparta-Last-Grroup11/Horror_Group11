2371728b172dce721f4ec504d5f7925a5a249d72
tmdwp <tmdwp1010@gmail.com>
2025-04-24 12:46:38 +0900

    Fix 금고 및 발전기 코드 수정
    매니저 접근 없이 실행되도록 수정

--- Assets/Script/Interact/PowerOn.cs
+++ Assets/Script/Interact/PowerOn.cs
@@ -4,25 +4,13 @@ using UnityEngine;
 
 public class PowerOn : MonoBehaviour, I_Interactable
 {
-    [SerializeField] private RoomsLightManage roomManage;
     [SerializeField] private float flash = 0.2f;
     [SerializeField] private bool isAct;
+    [SerializeField] private TurnLight lights;
     public void OnInteraction()
     {
         if (isAct) return;
         isAct = true;
-        PuzzleManager.Instance.OnPower();
-        //StartCoroutine(Blink());
-    }
-
-    IEnumerator Blink()
-    {
-        for (int i = 0; i < 2; i++)
-        {
-            roomManage.TurnBMLight();
-            yield return new WaitForSeconds(flash++);
-            roomManage.TurnBMLight();
-            yield return new WaitForSeconds(flash++);
-        }
+        lights.OnPower();
     }
 }
