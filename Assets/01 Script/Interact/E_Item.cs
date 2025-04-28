ca3e9776c337ca3a3602e94c4419bec73c566377
tmdwp <tmdwp1010@gmail.com>
2025-04-28 17:53:33 +0900

    Feat 의식 퍼즐 구현
    의식 퍼즐 구현 및 전등 off 시 몬스터에게 신호를 줄 SO 구현

--- Assets/01 Script/Interact/E_Item.cs
+++ Assets/01 Script/Interact/E_Item.cs
@@ -1,7 +1,7 @@
 public enum E_Item
 {
     None = -1,
-    Battery = 0, BoxInDiningRoom,
+    Battery = 0, BoxInDiningRoom, Offering1, Offering2, Offering3, Offering4, Offering5,
     PowerRoomKey = 100, MovieRoomKey, FrontDoorKey, SurgeryRoomKey, TortureRoomKey, EmptyRoomKey,
     FileA = 200, FileB, MemoInFront, MemoInOffice,
 }
