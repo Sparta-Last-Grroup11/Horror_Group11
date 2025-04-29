2974d15c430175bcdb02815a1b0c01b4c8869a6f
cookie35 <hakyeong35@gmail.com>
2025-04-29 15:37:26 +0900

    Refactor: 폴더 정리

--- Assets/01 Script/Enemy/03 SkinLessZombie/Trigger/TriggerGroupData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-[System.Serializable]
-public class TriggerGroupData
-{
-    public int stage;  // 몇 번째 스테이지인지
-    public List<int> activeIndices;  // 켜질 Observer 인덱스들
-}
