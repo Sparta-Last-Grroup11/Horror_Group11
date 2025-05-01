74eca8e8950cd8806700068d4174646f8e1f1994
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-05-01 17:33:34 +0900

    Fix: 스타트씬 수정

--- Assets/01 Script/UI/StartScene/StartUI.cs
+++ Assets/01 Script/UI/StartScene/StartUI.cs
@@ -6,6 +6,7 @@ public class StartUI : MonoBehaviour
 {
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.None;
         AudioManager.Instance.AudioBGMPlay(ResourceManager.Instance.Load<AudioClip>(ResourceType.Sound, "StartSceneBGM"), true, 0.7f, EAudioType.BGM);
         UIManager.Instance.show<StartSceneUI>();
     }
