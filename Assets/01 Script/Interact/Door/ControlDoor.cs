91f49609910f4dd2890d6a36efef8104a927fcf3
tmdwp <tmdwp1010@gmail.com>
2025-05-08 17:30:57 +0900

    Fix: 문 여닫기 수정

--- Assets/01 Script/Interact/Door/ControlDoor.cs
+++ Assets/01 Script/Interact/Door/ControlDoor.cs
@@ -6,38 +6,52 @@ using static UnityEditor.PlayerSettings;
 public class ControlDoor : OpenableObject
 {
     public float duration = 1f;
+    private Quaternion openRotation;
+    private Quaternion closeRotation;
     [SerializeField] private float openDegree = 90;
+
+    private void Awake()
+    {
+        SetRotation();
+    }
+    protected void SetRotation()
+    {
+        closeRotation = transform.rotation;
+        openRotation = closeRotation * Quaternion.Euler(0, openDegree, 0); ;
+    }
     protected override IEnumerator OpenRoutine() //문 회전
     {
-        Quaternion startRotation = transform.rotation;
-        Quaternion endRotation = startRotation * Quaternion.Euler(0, openDegree, 0);
-
         float elapsed = 0f;
-
-        while (elapsed < duration)
+        try
         {
-            transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsed / duration);
-            elapsed += Time.deltaTime;
-            yield return null;
+            while (elapsed < duration)
+            {
+                transform.rotation = Quaternion.Slerp(closeRotation, openRotation, elapsed / duration);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
         }
-
-        transform.rotation = endRotation;
+        finally
+        {
+            transform.rotation = openRotation;
+        }
     }
 
     protected override IEnumerator CloseRoutine() //문 회전
     {
-        Quaternion startRotation = transform.rotation;
-        Quaternion endRotation = startRotation * Quaternion.Euler(0, -openDegree, 0);
-
         float elapsed = 0f;
-
-        while (elapsed < duration)
+        try
         {
-            transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsed / duration);
-            elapsed += Time.deltaTime;
-            yield return null;
+            while (elapsed < duration)
+            {
+                transform.rotation = Quaternion.Slerp(openRotation, closeRotation, elapsed / duration);
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
         }
-
-        transform.rotation = endRotation;
+        finally
+        {
+            transform.rotation = closeRotation;
+        }
     }
 }
