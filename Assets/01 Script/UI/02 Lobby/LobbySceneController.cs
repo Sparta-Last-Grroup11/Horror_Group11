0f8dac6b22f6364e8dfae4912c5d6a97aae14a15
psj1208 <162458734+psj1208@users.noreply.github.com>
2025-05-22 12:10:14 +0900

    Fix: 오디오 설정 오류 및 초기에 도움말

--- Assets/01 Script/UI/02 Lobby/LobbySceneController.cs
+++ Assets/01 Script/UI/02 Lobby/LobbySceneController.cs
@@ -7,11 +7,29 @@ public class LobbySceneController : MonoBehaviour
     private void Start()
     {
         UIManager.Instance.show<Interacting>();
+        Invoke(nameof(ShowHowtoControl), 1f);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) && UIManager.Instance.Get<ExitUI>() == null)
             UIManager.Instance.show<ExitUI>();
+    }
+
+    void ShowHowtoControl()
+    {
+        UIManager.Instance.show<HelpUI>().ActivePanel(0);
+        StartCoroutine(HelpUIExit());
+    }
+
+    IEnumerator HelpUIExit()
+    {
+        while (UIManager.Instance.Get<HelpUI>() != null)
+        {
+            yield return null;
+        }
+        UIManager.Instance.show<PopUpText>().Init("You Can see this Panel in Exit - Help");
+        AudioManager.Instance.Audio2DPlay(ResourceManager.Instance.Load<AudioClip>(ResourceType.Sound, "2D/UI/ButtonClick"));
+        yield return null;
     }
 }
