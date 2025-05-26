619fa780748d398e4b1505d55650c980ec0dc53f
Najaehyeon <jaehyeon4832@gmail.com>
2025-05-26 17:27:27 +0900

    Feat: 프롤로그 스킵 기능 추가

--- Assets/01 Script/UI/01 Start/PrologueUI.cs
+++ Assets/01 Script/UI/01 Start/PrologueUI.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
@@ -15,58 +17,83 @@ public class PrologueUI : PopupUI
     [SerializeField] AudioClip typingClip;
     [SerializeField] AudioClip prologueBGM;
     [SerializeField] private TMP_Text targetText;
+
     private float delay = 0.1f;
     private float pauseBetweenSentences = 1f;
 
+    private CancellationTokenSource cts;
+
     async void Start()
     {
-        await PrintAllSentencesAsync();
+        cts = new CancellationTokenSource();
+
+        // SkipUI 표시 및 스킵 완료 시 토큰 취소 설정
+        UIManager.Instance.show<SkipUI>().Init(1f, () =>
+        {
+            cts.Cancel();
+        });
+
+        try
+        {
+            await PrintAllSentencesAsync(cts.Token);
+        }
+        catch (OperationCanceledException)
+        {
+            Debug.Log("Skipped prologue.");
+        }
+
+        await SceneLoadManager.Instance.ChangeScene("LobbyScene");
     }
 
-    async Task PrintAllSentencesAsync()
+    async Task PrintAllSentencesAsync(CancellationToken token)
     {
         AudioManager.Instance.StopBGM();
         AudioManager.Instance.Audio2DPlay(prologueBGM, 1, false, EAudioType.BGM);
         AudioManager.Instance.Audio2DPlay(typingClip, 1, false, EAudioType.SFX);
-        await Task.Delay(1000);
+        await Task.Delay(1000, token);
 
-        await TypeSentenceAsync(firstSentence);
+        await TypeSentenceAsync(firstSentence, token);
         AudioManager.Instance.Audio2DPlay(typingClip, 1, false, EAudioType.SFX);
-        await Task.Delay((int)(pauseBetweenSentences * 1000));
+        await Task.Delay((int)(pauseBetweenSentences * 1000), token);
 
-        await TypeSentenceAsync(secondSentence);
+        await TypeSentenceAsync(secondSentence, token);
         AudioManager.Instance.Audio2DPlay(typingClip, 1, false, EAudioType.SFX);
-        await Task.Delay((int)(pauseBetweenSentences * 1000));
+        await Task.Delay((int)(pauseBetweenSentences * 1000), token);
 
-        await TypeSentenceAsync(thirdSentence);
+        await TypeSentenceAsync(thirdSentence, token);
         AudioManager.Instance.Audio2DPlay(typingClip, 1, false, EAudioType.SFX);
-        await Task.Delay((int)(pauseBetweenSentences * 1000));
+        await Task.Delay((int)(pauseBetweenSentences * 1000), token);
 
-        await TypeSentenceAsync(fourthSentence);
+        await TypeSentenceAsync(fourthSentence, token);
         AudioManager.Instance.Audio2DPlay(typingClip, 1, false, EAudioType.SFX);
-        await Task.Delay((int)(pauseBetweenSentences * 1000));
+        await Task.Delay((int)(pauseBetweenSentences * 1000), token);
 
-        await TypeSentenceAsync(fifthSentence);
+        await TypeSentenceAsync(fifthSentence, token);
         AudioManager.Instance.Audio2DPlay(typingClip, 1, false, EAudioType.SFX);
-        await Task.Delay((int)(pauseBetweenSentences * 1000));
+        await Task.Delay((int)(pauseBetweenSentences * 1000), token);
 
-        await TypeSentenceAsync(sixthSentence);
+        await TypeSentenceAsync(sixthSentence, token);
         AudioManager.Instance.Audio2DPlay(typingClip, 1, false, EAudioType.SFX);
 
         targetText.DOFade(0, 8f);
-        await Task.Delay(8000); // targetText가 페이드 아웃되는 시간
-
-        await SceneLoadManager.Instance.ChangeScene("LobbyScene"); // 이 앞에 await 가능
+        await Task.Delay(8000, token);
     }
 
-    async Task TypeSentenceAsync(string sentence)
+    async Task TypeSentenceAsync(string sentence, CancellationToken token)
     {
         targetText.text = "";
 
         foreach (char letter in sentence)
         {
+            token.ThrowIfCancellationRequested();
             targetText.text += letter;
-            await Task.Delay((int)(delay * 1000));
+            await Task.Delay((int)(delay * 1000), token);
         }
+    }
+
+    private void OnDestroy()
+    {
+        cts?.Cancel();
+        cts?.Dispose();
     }
 }
