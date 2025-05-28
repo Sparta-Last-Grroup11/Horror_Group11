d63688feded9bd8215e553a66c51258b8e44a871
Najaehyeon <jaehyeon4832@gmail.com>
2025-05-28 09:51:38 +0900

    Feat: 엔딩 컷씬 추가

--- Assets/01 Script/Event/Core/EndingSceneTrigger.cs
+++ Assets/01 Script/Event/Core/EndingSceneTrigger.cs
@@ -23,6 +23,14 @@ public class EndingSceneTrigger : MonoBehaviour
             AudioManager.Instance.Audio2DPlay(endingCutAudioClip, 1, false, EAudioType.BGM);
             endingCutSceneVirtualCamera.Priority = 14;
             endingCutSceneDirector.Play();
+            StartCoroutine(Ending());
         }
+    }
+
+    IEnumerator Ending()
+    {
+        yield return new WaitForSeconds(10f);
+        MonologueManager.Instance.DialogPlay(8);
+        UIManager.Instance.show<EndGameUI>().ShowEnding(EndingCategory.Escape, 1000);
     }
 }
